9af4dbe40a44eb1ecc429164e0fcd9c22149bfc8
CalebHatch <calebhatchcontact@gmail.com>
2022-01-13 04:14:17 -0600

    Polished some things

--- C-to-F/main.cpp
+++ C-to-F/main.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main(){
     double celsiusTemp;
-    cout << "This program converts a temperature in degrees Celsius into a temperature in degrees Fahrenheit. Enter a temperature in degrees Celsius: ";
+    cout << "This program converts a temperature in degrees Celsius into a temperature in degrees Fahrenheit. Enter a temperature in degrees Celsius: " << endl;
     cin >> celsiusTemp;
 
     double fahrenheitTemp;
     fahrenheitTemp = (celsiusTemp * 1.8) + 32;
 
-    cout << celsiusTemp << " degrees is equal to " << fahrenheitTemp << " degrees Fahrenheit. Goodbye.";
+    cout << celsiusTemp << " degrees Celsius is equal to " << fahrenheitTemp << " degrees Fahrenheit. Goodbye." << endl;
 
     system("pause");
     return 0;
